d8ad76250efea7bc6d62321c174fb4ace48211b0
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-07 00:12:00 +0630

    First API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 export default {
-  async fetch() {
-    return new Response("Tartay 2D API is running 🚀");
-  },
-};
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/") {
+      return Response.json({
+        app: "Tartay 2D",
+        status: "Online",
+        version: "1.0.0"
+      });
+    }
+
+    return new Response("Not Found", {
+      status: 404
+    });
+  }
+}
