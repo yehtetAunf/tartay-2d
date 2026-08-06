967aa3d32be1eeb452ee66e237a7ba49328bd93d
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-07 00:28:25 +0630

    Add env parameter to fetch function and implement login

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export default {
-  async fetch(request: Request): Promise<Response> {
+  async fetch(request: Request, env: any): Promise<Response> {
     const url = new URL(request.url);
 
+    // Home
     if (url.pathname === "/") {
       return Response.json({
         app: "Tartay 2D",
@@ -10,8 +11,34 @@ export default {
       });
     }
 
+    // Login API
+    if (url.pathname === "/login" && request.method === "POST") {
+      const body = await request.json();
+
+      const username = body.username;
+      const password = body.password;
+
+      const result = await env.DB.prepare(
+        "SELECT * FROM users WHERE username = ?"
+      )
+        .bind(username)
+        .first();
+
+      if (!result) {
+        return Response.json({
+          success: false,
+          message: "User not found"
+        });
+      }
+
+      return Response.json({
+        success: true,
+        user: result
+      });
+    }
+
     return new Response("Not Found", {
       status: 404
     });
   }
-}
+};
