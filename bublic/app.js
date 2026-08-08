183a99a038bdcf60df89575d3b28aefdff401f85
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-08 22:56:10 +0630

    Delete bublic directory

--- bublic/app.js
+++
@@ -1,3 +0,0 @@
-const times=['05:00 PM','06:00 PM','07:00 PM','08:00 PM','09:00 PM','10:00 PM','11:00 PM','12:00 AM'];
-document.querySelector('#rounds').innerHTML=times.map(t=>`<article><b>${t}</b><span>--</span></article>`).join('');
-fetch('/api/status').catch(()=>{});
