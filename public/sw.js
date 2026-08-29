4d5ac4b312a02c568d68b20111a1fdaac198d1a8
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-29 19:14:11 +0630

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tartay-2d-v7-2-clean-header';
+const CACHE='tartay-2d-v8-2-clean-header';
 const ASSETS=['/','/app.css?v=7.2','/app.js?v=7.2','/haunted-clear.png?v=6.33','/history.html','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
